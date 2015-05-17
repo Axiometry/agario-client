ded8a78610471a70e21f5a14df54c2af2e27ee55
Axiometry <marco@vanhil.st>
2015-05-17 01:39:03 -0400

    Server listing

--- src/main/scala/me/axiometry/agario/Agario.scala
+++ src/main/scala/me/axiometry/agario/Agario.scala
@@ -1,8 +1,87 @@
 package me.axiometry.agario
 
+import akka.io.IO
+import akka.pattern.ask
 import akka.actor.ActorSystem
+import akka.util.Timeout
 
-object Agario extends App {
+import spray.can.Http
+import spray.http._
+import HttpHeaders._
+import HttpMethods._
+import MediaTypes._
+import spray.client.pipelining._
+
+import scala.concurrent._
+import scala.concurrent.duration._
+import scala.util.{ Try, Failure, Success }
+
+import grizzled.slf4j.Logging
+
+
+object Agario extends Logging {
   implicit val system = ActorSystem()
+  import system.dispatcher
 
+  private[this] implicit val timeout: Timeout = 10 seconds
+  private[this] val ServerPattern = "<option value=\"([a-zA-Z]+)\\-([a-zA-Z]+)\">[a-zA-Z ]+</option>".r
+
+  def servers: List[Server] = {
+    val submit: HttpRequest => Future[HttpResponse] = sendReceive
+    val response: Future[HttpResponse] = submit(Get("http://agar.io/"))
+
+    val page = Try(Await.ready(response, timeout.duration)) match {
+      case Success(_) if response.value.get.isSuccess =>
+        val res = response.value.get.get
+        Some(res.entity.data.asString)
+      case Success(_) => response.value.get match {
+        case Failure(error) =>
+          debug("Request to agar.io failed", error)
+          None
+        case _ => None
+      }
+      case Failure(error) =>
+        debug("Request to agar.io failed", error)
+        None
+      case _ => None
+    }
+
+    debug(s"Page: $page")
+
+    page match {
+      case Some(page) =>
+        (page split '\n' toList) flatMap {
+          case ServerPattern(region, name) => serverAddr(region, name).map(addr => Server(region, name, addr))
+          case _ => None
+        }
+      case None => List()
+    }
+  }
+
+  private[this] def serverAddr(region: String, name: String): Option[ServerAddress] = {
+    debug(s"Sending request about $region-$name")
+    val submit: HttpRequest => Future[HttpResponse] = sendReceive
+    val response: Future[HttpResponse] = submit(Post("http://m.agar.io/", s"$region-$name"))
+
+    Try(Await.ready(response, timeout.duration)) match {
+      case Success(_) if response.value.get.isSuccess =>
+        val res = response.value.get.get
+        res.entity.data.asString.split('\n')(0) match {
+          case ServerAddress(addr) => Some(addr)
+          case str =>
+            debug(s"Not an address: $str")
+            None
+        }
+      case Success(_) => response.value.get match {
+        case Failure(error) =>
+          debug("Request to m.agar.io failed", error)
+          None
+        case _ => None
+      }
+      case Failure(error) =>
+        debug("Request to m.agar.io failed (no response)", error)
+        None
+      case _ => None
+    }
+  }
 }
--- src/main/scala/me/axiometry/agario/Server.scala
+++ src/main/scala/me/axiometry/agario/Server.scala
@@ -1,7 +1,20 @@
 package me.axiometry.agario
 
 import akka.actor.ActorSystem
+import scala.util._
 
-case class Server(val region: String, val name: String, val address: String)(implicit system: ActorSystem) {
+case class ServerAddress(host: String, port: Int)
+
+object ServerAddress {
+  private[this] val ServerPattern = "([^:]+):([0-9]+)".r
+  def unapply(string: String): Option[ServerAddress] = string match {
+    case ServerPattern(host, port) => Try(port.toInt) match {
+      case Success(port) => Some(ServerAddress(host, port))
+      case Failure(_) => None
+    }
+    case _ => None
+  }
+}
+case class Server(val region: String, val name: String, val address: ServerAddress)(implicit system: ActorSystem) {
   def createClient(name: String = null): Client = ???
 }
