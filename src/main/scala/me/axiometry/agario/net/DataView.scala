8708ef82d1a4663c859b83f5f8f3fdd37b899825
Axiometry <marco@vanhil.st>
2015-05-17 03:23:57 -0400

    DataView: add apply(Array[Byte]); fix default implicit byteOrder on functions

--- src/main/scala/me/axiometry/agario/net/DataView.scala
+++ src/main/scala/me/axiometry/agario/net/DataView.scala
@@ -3,17 +3,17 @@ package me.axiometry.agario.net
 case class DataView(array: Array[Byte], offset: Int, length: Int)(implicit val byteOrder: ByteOrder = ByteOrder.BigEndian) {
   import ByteOrder._
 
-  def getInt8(off: Int)(implicit byteOrder: ByteOrder): Byte =
+  def getInt8(off: Int)(implicit byteOrder: ByteOrder = byteOrder): Byte =
     array(off)
-  def setInt8(off: Int, value: Byte)(implicit byteOrder: ByteOrder): Unit =
+  def setInt8(off: Int, value: Byte)(implicit byteOrder: ByteOrder = byteOrder): Unit =
     array(off) = value
 
-  def getUint8(off: Int)(implicit byteOrder: ByteOrder): Int =
+  def getUint8(off: Int)(implicit byteOrder: ByteOrder = byteOrder): Int =
     array(off) & 0xFF
-  def setUint8(off: Int, value: Int)(implicit byteOrder: ByteOrder): Unit =
+  def setUint8(off: Int, value: Int)(implicit byteOrder: ByteOrder = byteOrder): Unit =
     array(off) = value.toByte
 
-  def getInt16(off: Int)(implicit byteOrder: ByteOrder): Short = byteOrder match {
+  def getInt16(off: Int)(implicit byteOrder: ByteOrder = byteOrder): Short = byteOrder match {
     case BigEndian =>
       ((array(off).toInt << 8)
           + array(off+1).toInt).toShort
@@ -21,7 +21,7 @@ case class DataView(array: Array[Byte], offset: Int, length: Int)(implicit val b
       ((array(off).toInt << 8)
           + array(off).toInt).toShort
   }
-  def setInt16(off: Int, value: Short)(implicit byteOrder: ByteOrder): Unit = byteOrder match {
+  def setInt16(off: Int, value: Short)(implicit byteOrder: ByteOrder = byteOrder): Unit = byteOrder match {
     case BigEndian =>
       array(off) = ((value >>> 8) & 0xFF).toByte
       array(off+1) = (value & 0xFF).toByte
@@ -30,7 +30,7 @@ case class DataView(array: Array[Byte], offset: Int, length: Int)(implicit val b
       array(off) = (value & 0xFF).toByte
   }
 
-  def getUint16(off: Int)(implicit byteOrder: ByteOrder): Int = byteOrder match {
+  def getUint16(off: Int)(implicit byteOrder: ByteOrder = byteOrder): Int = byteOrder match {
     case BigEndian =>
       ((array(off).toInt << 8)
           + array(off+1))
@@ -38,7 +38,7 @@ case class DataView(array: Array[Byte], offset: Int, length: Int)(implicit val b
       ((array(off+1).toInt << 8)
           + array(off))
   }
-  def setUint16(off: Int, value: Int)(implicit byteOrder: ByteOrder): Unit = byteOrder match {
+  def setUint16(off: Int, value: Int)(implicit byteOrder: ByteOrder = byteOrder): Unit = byteOrder match {
     case BigEndian =>
       array(off) = ((value >>> 8) & 0xFF).toByte
       array(off+1) = (value & 0xFF).toByte
@@ -47,7 +47,7 @@ case class DataView(array: Array[Byte], offset: Int, length: Int)(implicit val b
       array(off) = (value & 0xFF).toByte
   }
 
-  def getInt32(off: Int)(implicit byteOrder: ByteOrder): Int = byteOrder match {
+  def getInt32(off: Int)(implicit byteOrder: ByteOrder = byteOrder): Int = byteOrder match {
     case BigEndian =>
       ((array(off).toInt << 24)
           + (array(off+1).toInt << 16)
@@ -59,7 +59,7 @@ case class DataView(array: Array[Byte], offset: Int, length: Int)(implicit val b
           + (array(off+1).toInt << 8)
           + array(off).toInt)
   }
-  def setInt32(off: Int, value: Int)(implicit byteOrder: ByteOrder): Unit = byteOrder match {
+  def setInt32(off: Int, value: Int)(implicit byteOrder: ByteOrder = byteOrder): Unit = byteOrder match {
     case BigEndian =>
       array(off) = ((value >>> 24) & 0xFF).toByte
       array(off+1) = ((value >>> 16) & 0xFF).toByte
@@ -72,7 +72,7 @@ case class DataView(array: Array[Byte], offset: Int, length: Int)(implicit val b
       array(off) = (value & 0xFF).toByte
   }
 
-  def getUint32(off: Int)(implicit byteOrder: ByteOrder): Long = byteOrder match {
+  def getUint32(off: Int)(implicit byteOrder: ByteOrder = byteOrder): Long = byteOrder match {
     case BigEndian =>
       ((array(off).toLong << 24)
           + (array(off+1).toLong << 16)
@@ -84,7 +84,7 @@ case class DataView(array: Array[Byte], offset: Int, length: Int)(implicit val b
           + (array(off+1).toLong << 8)
           + array(off).toLong)
   }
-  def setUint32(off: Int, value: Long)(implicit byteOrder: ByteOrder): Unit = byteOrder match {
+  def setUint32(off: Int, value: Long)(implicit byteOrder: ByteOrder = byteOrder): Unit = byteOrder match {
     case BigEndian =>
       array(off) = ((value >>> 24) & 0xFF).toByte
       array(off+1) = ((value >>> 16) & 0xFF).toByte
@@ -97,18 +97,19 @@ case class DataView(array: Array[Byte], offset: Int, length: Int)(implicit val b
       array(off) = (value & 0xFF).toByte
   }
 
-  def getFloat32(off: Int)(implicit byteOrder: ByteOrder): Float =
+  def getFloat32(off: Int)(implicit byteOrder: ByteOrder = byteOrder): Float =
     java.lang.Float.intBitsToFloat(getInt32(off))
-  def setFloat32(off: Int, value: Float)(implicit byteOrder: ByteOrder): Unit =
+  def setFloat32(off: Int, value: Float)(implicit byteOrder: ByteOrder = byteOrder): Unit =
     setInt32(off, java.lang.Float.floatToRawIntBits(value))
 
-  def getFloat64(off: Int)(implicit byteOrder: ByteOrder): Double =
+  def getFloat64(off: Int)(implicit byteOrder: ByteOrder = byteOrder): Double =
     java.lang.Double.longBitsToDouble(getUint32(off))
-  def setFloat64(off: Int, value: Double)(implicit byteOrder: ByteOrder): Unit =
+  def setFloat64(off: Int, value: Double)(implicit byteOrder: ByteOrder = byteOrder): Unit =
     setUint32(off, java.lang.Double.doubleToRawLongBits(value))
 }
 object DataView {
   def apply(length: Int): DataView = DataView(Array.ofDim(length), 0, length)
+  def apply(array: Array[Byte]): DataView = DataView(array, 0, array.length)
 }
 
 sealed trait ByteOrder
